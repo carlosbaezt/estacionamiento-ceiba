0e24de22107ac13fee1d482eef14f73ec1ffa488
Carlos Eduardo Baez - Ceiba Software <carlos.baez@ceiba.com.co>
2018-11-06 11:21:37 -0500

    se anade el log a trcmservice

--- src/main/java/com/ceiba/estacionamiento_api/services/TcrmService.java
+++ src/main/java/com/ceiba/estacionamiento_api/services/TcrmService.java
@@ -1,9 +1,12 @@
 package com.ceiba.estacionamiento_api.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.ceiba.estacionamiento_api.controllers.VigilanteController;
 import com.ceiba.estacionamiento_api.exceptions.TcrmException;
 import com.ceiba.estacionamiento_api.models.Tcrm;
 
@@ -11,6 +14,7 @@ import com.ceiba.estacionamiento_api.models.Tcrm;
 public class TcrmService{
 	
 	private static final String URL_REST = "http://www.set-fx.com/stats";
+	private static final Logger LOGGER = LoggerFactory.getLogger(TcrmService.class);
 		
 	public Tcrm consultarTcrm() throws TcrmException
 	{
@@ -18,9 +22,10 @@ public class TcrmService{
 		{
 			RestTemplate restTemplate = new RestTemplate();
 	        return restTemplate.getForObject(URL_REST, Tcrm.class);
-		}catch( RestClientException e)
+		}catch( RestClientException exception)
 		{
-			throw new TcrmException(e.getMessage());
+			LOGGER.info("Error en /obtenerTrm",exception);
+			throw new TcrmException(exception.getMessage());
 		}
 	}
 }
